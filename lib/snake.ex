2ce9c6272c0360962c2e30dfb1ee74497bcc9373
Yuri Artemev <i@artemeff.com>
2020-02-12 19:21:42 +0300

    init

---
+++ lib/snake.ex
@@ -0,0 +1,18 @@
+defmodule Snake do
+  @moduledoc """
+  Documentation for `Snake`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Snake.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

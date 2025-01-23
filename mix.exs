46a36c892063aa5663541057cb6db96d274696dd
Yuri Artemev <i@artemeff.com>
2025-01-23 18:50:22 +0700

    release 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ouroboros.MixProject do
   def project do
     [
       app: :ouroboros,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,

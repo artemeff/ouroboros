3cec129cb8b5550b9c2b16031d6648727d52c5ad
Alexander Samoylenko <lxmnk.inbox@gmail.com>
2025-01-23 18:31:00 +0700

    fix: Keyword.keys() cannot be used for non-keyword lists from Elixir 1.11

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -4,7 +4,10 @@ defmodule Ouroboros.Repo do
 
   def init(_type, config) do
     {:ok, Keyword.merge(config, [
+      username: "postgres",
+      password: "postgres",
       database: "ouroboros_db",
+      hostname: "localhost",
       pool: Ecto.Adapters.SQL.Sandbox
     ])}
   end
